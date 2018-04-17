5ed3c8244784d30ac7110ada4f8d18e92a3e0ad0
Reginald Johnson <reginald.johnson@gmail.com>
2018-04-17 18:14:41 -0400

    first commit

---
+++ packages/aws-appsync/src/client.js
@@ -0,0 +1,170 @@
+/*!
+ * Copyright 2017-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Licensed under the Amazon Software License (the "License"). You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *     http://aws.amazon.com/asl/
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+import 'setimmediate';
+import ApolloClient, { ApolloClientOptions, MutationOptions } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink, FetchResult, Observable } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { getMainDefinition, getOperationDefinition, variablesInOperation } from 'apollo-utilities';
+
+import OfflineCache from './cache/index';
+import { OfflineLink, AuthLink, NonTerminatingHttpLink, SubscriptionHandshakeLink, ComplexObjectLink } from './link';
+import { createStore } from './store';
+
+export const createSubscriptionHanshakeLink = (url, nonSubscriptionLink = new HttpLink({ uri: url })) => {
+    return ApolloLink.split(
+        operation => {
+            const { query } = operation;
+            const { kind, operation: graphqlOperation } = getMainDefinition(query);
+            const isSubscription = kind === 'OperationDefinition' && graphqlOperation === 'subscription';
+
+            return isSubscription;
+        },
+        ApolloLink.from([
+            new NonTerminatingHttpLink('subsInfo', { uri: url }, true),
+            new SubscriptionHandshakeLink('subsInfo'),
+        ]),
+        nonSubscriptionLink,
+    );
+};
+
+export const createAuthLink = ({ url, region, auth }) => new AuthLink({ url, region, auth });
+
+const passthrough = (op, forward) => (forward ? forward(op) : Observable.of());
+
+export const createAppSyncLink = ({
+    url,
+    region,
+    auth,
+    store,
+    complexObjectsCredentials,
+    beforeFetcherLink,
+}) => {
+    const link = ApolloLink.from([
+        new ApolloLink(passthrough),
+        store && new OfflineLink(store),
+        new ComplexObjectLink(complexObjectsCredentials),
+        beforeFetcherLink,
+        createAuthLink({ url, region, auth }),
+        createSubscriptionHanshakeLink(url)
+    ].filter(Boolean));
+
+    return link;
+};
+
+export const createLinkWithCache = (createLinkFunc = () => new ApolloLink(passthrough)) => {
+    return new ApolloLink((op, forward) => {
+        const { cache } = op.getContext();
+
+        const theLink = createLinkFunc(cache);
+
+        return theLink.request(op, forward);
+    });
+}
+
+class AWSAppSyncClient extends ApolloClient {
+
+    /**
+     * @type {Promise<AWSAppSyncClient>}
+     */
+    hydratedPromise;
+
+    hydrated = () => this.hydratedPromise;
+
+    /**
+     *
+     * @param {object} appSyncOptions
+     * @param {ApolloClientOptions<InMemoryCache>} options
+     */
+    constructor({
+        url,
+        region,
+        auth,
+        conflictResolver,
+        complexObjectsCredentials,
+        cacheOptions,
+        disableOffline = false
+    } = {}, options = {}) {
+        const { cache: customCache, link: customLink } = options;
+
+        if (!customLink && (!url || !region || !auth)) {
+            throw new Error(
+                'In order to initialize AWSAppSyncClient, you must specify url, region and auth properties on the config object.'
+            );
+        }
+
+        let resolveClient;
+        this.hydratedPromise = disableOffline ? Promise.resolve(this) : new Promise(resolve => resolveClient = resolve);
+
+        const store = disableOffline ? null : createStore(this, () => resolveClient(this), conflictResolver);
+        const cache = disableOffline ? (customCache || new InMemoryCache(cacheOptions)) : new OfflineCache(store, cacheOptions);
+
+        const waitForRehydrationLink = new ApolloLink((op, forward) => {
+            let handle = null;
+
+            return new Observable(observer => {
+                this.hydratedPromise.then(() => {
+                    handle = passthrough(op, forward).subscribe(observer);
+                }).catch(observer.error);
+
+                return () => {
+                    if (handle) {
+                        handle.unsubscribe();
+                    }
+                };
+            });
+        });
+        const link = waitForRehydrationLink.concat(customLink || createAppSyncLink({ url, region, auth, store, complexObjectsCredentials }));
+
+        const newOptions = {
+            ...options,
+            link,
+            cache,
+        };
+
+        super(newOptions);
+    }
+
+    /**
+     *
+     * @param {MutationOptions} options
+     * @returns {Promise<FetchResult>}
+     */
+    mutate(options) {
+        const { update, refetchQueries, context: origContext = {}, ...otherOptions } = options;
+        const { AASContext: { doIt = false, ...restAASContext } = {} } = origContext;
+
+        const context = {
+            ...origContext,
+            AASContext: {
+                doIt,
+                ...restAASContext,
+                ...(!doIt ? { refetchQueries, update } : {}),
+            }
+        };
+
+        const { optimisticResponse, variables } = otherOptions;
+        const data = optimisticResponse &&
+            (typeof optimisticResponse === 'function' ? { ...optimisticResponse(variables) } : optimisticResponse);
+
+        const newOptions = {
+            ...otherOptions,
+            optimisticResponse: data,
+            update,
+            ...(doIt ? { refetchQueries } : {}),
+            context,
+        }
+
+        return super.mutate(newOptions);
+    }
+
+};
+
+export default AWSAppSyncClient;
+export { AWSAppSyncClient };
---
+++ packages/aws-appsync/src/link/offline-link.js
@@ -0,0 +1,231 @@
+/*!
+ * Copyright 2017-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Licensed under the Amazon Software License (the "License"). You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *     http://aws.amazon.com/asl/
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+import { readQueryFromStore, defaultNormalizedCacheFactory } from "apollo-cache-inmemory";
+import { ApolloLink, Observable, Operation } from "apollo-link";
+import { getOperationDefinition, getOperationName } from "apollo-utilities";
+import { Store } from "redux";
+
+import { NORMALIZED_CACHE_KEY } from "../cache";
+
+export class OfflineLink extends ApolloLink {
+
+    /**
+     * @type {Store}
+     * @private
+     */
+    store;
+
+    /**
+     *
+     * @param {Store} store
+     */
+    constructor(store) {
+        this.store = store;
+    }
+
+    request(operation, forward) {
+        return new Observable(observer => {
+            const { offline: { online } } = this.store.getState();
+            const { operation: operationType } = getOperationDefinition(operation.query);
+            const isMutation = operationType === 'mutation';
+            const isQuery = operationType === 'query';
+
+            if (!online && isQuery) {
+                const data = processOfflineQuery(operation, this.store);
+
+                observer.next({ data });
+                observer.complete();
+
+                return () => null;
+            }
+
+            if (isMutation) {
+                const { cache, optimisticResponse, AASContext: { doIt = false } = {} } = operation.getContext();
+
+                if (!doIt) {
+                    if (!optimisticResponse) {
+                        console.warn('An optimisticResponse was not provided, it is required when using offline capabilities.');
+
+                        if (!online) {
+                            throw new Error('Missing optimisticResponse while offline.');
+                        }
+
+                        // offline muation without optimistic response is processed immediately
+                    } else {
+                        const data = enqueueMutation(operation, this.store, observer);
+
+                        observer.next({ data });
+                        observer.complete();
+
+                        return () => null;
+                    }
+                }
+            }
+
+            const handle = forward(operation).subscribe({
+                next: observer.next.bind(observer),
+                error: observer.error.bind(observer),
+                complete: observer.complete.bind(observer),
+            });
+
+            return () => {
+                if (handle) handle.unsubscribe();
+            };
+        });
+    }
+}
+
+/**
+ *
+ * @param {Operation} operation
+ * @param {Store} theStore
+ */
+const processOfflineQuery = (operation, theStore) => {
+    const { [NORMALIZED_CACHE_KEY]: normalizedCache = {} } = theStore.getState();
+    const { query, variables } = operation;
+
+    const store = defaultNormalizedCacheFactory(normalizedCache);
+
+    const data = readQueryFromStore({
+        store,
+        query,
+        variables,
+    });
+
+    return data;
+}
+
+/**
+ *
+ * @param {Operation} operation
+ * @param {Store} theStore
+ */
+const enqueueMutation = (operation, theStore, observer) => {
+    const { query: mutation, variables } = operation;
+    const { cache, optimisticResponse, AASContext: { doIt = false, refetchQueries, update } = {} } = operation.getContext();
+
+    setImmediate(() => {
+        theStore.dispatch({
+            type: 'SOME_ACTION',
+            payload: {},
+            meta: {
+                offline: {
+                    effect: {
+                        mutation,
+                        variables,
+                        refetchQueries,
+                        update,
+                        optimisticResponse,
+                    },
+                    commit: { type: 'SOME_ACTION_COMMIT', meta: null },
+                    rollback: { type: 'SOME_ACTION_ROLLBACK', meta: null },
+                }
+            }
+        });
+    });
+
+    return optimisticResponse;
+}
+
+/**
+ *
+ * @param {*} client
+ * @param {*} effect
+ * @param {*} action
+ */
+export const offlineEffect = (client, effect, action) => {
+    const doIt = true;
+    const { ...otherOptions } = effect;
+
+    const context = { AASContext: { doIt } };
+
+    const options = {
+        ...otherOptions,
+        context,
+    };
+
+    return client.mutate(options);
+}
+
+export const reducer = () => ({
+    eclipse: (state = {}, action) => {
+        const { type, payload } = action;
+        switch (type) {
+            case 'SOME_ACTION':
+                return {
+                    ...state,
+                };
+            case 'SOME_ACTION_COMMIT':
+                return {
+                    ...state,
+                };
+            case 'SOME_ACTION_ROLLBACK':
+                return {
+                    ...state,
+                };
+            default:
+                return state;
+        }
+    }
+});
+
+export const discard = (fn = () => null) => (error, action, retries) => {
+    const { graphQLErrors = [] } = error;
+    const conditionalCheck = graphQLErrors.find(err => err.errorType === 'DynamoDB:ConditionalCheckFailedException');
+
+    if (conditionalCheck) {
+        if (typeof fn === 'function') {
+            const { data, path } = conditionalCheck;
+            const { meta: { offline: { effect: { mutation, variables } } } } = action;
+            const mutationName = getOperationName(mutation);
+            const operationDefinition = getOperationDefinition(mutation)
+            const { operation: operationType } = operationDefinition;
+
+            try {
+                const conflictResolutionResult = fn({
+                    mutation,
+                    mutationName,
+                    operationType,
+                    variables,
+                    data,
+                    retries,
+                });
+
+                if (conflictResolutionResult === 'DISCARD') {
+                    return true;
+                }
+
+                if (!!conflictResolutionResult) {
+                    action.meta.offline.effect.variables = conflictResolutionResult;
+
+                    return false;
+                }
+            } catch (err) {
+                // console.error('Error running conflict resolution. Discarding mutation.', err);
+
+                return true;
+            }
+        }
+    } else if (graphQLErrors.length) {
+        // console.error('Discarding action.', action, graphQLErrors);
+
+        return true;
+    } else {
+        const { networkError: { graphQLErrors } = { graphQLErrors: [] } } = error;
+        const appSyncClientError = graphQLErrors.find(err => err.errorType && err.errorType.startsWith('AWSAppSyncClient:'));
+
+        if (appSyncClientError) {
+            // console.error('Discarding action.', action, appSyncClientError);
+
+            return true;
+        }
+    }
+
+    return error.permanent || retries > 10;
+};
